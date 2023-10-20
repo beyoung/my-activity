82662ff47d7cdce017a6e26f99c3f008e760d082
beyoung <1937583471@qq.com>
2023-10-20 22:11:00 +0800

    feature: update api

--- src/router.ts
+++ src/router.ts
@@ -4,15 +4,15 @@ import type { IRequest as IttyRequest, Route } from 'itty-router';
 import { Router } from 'itty-router';
 import { users } from './db/schema/users';
 import { records } from './db/schema/records';
-import { eq, lt, gte, ne, sql } from "drizzle-orm";
+import { eq, lt, gte, ne, sql } from 'drizzle-orm';
 
 interface Request extends IttyRequest {
 	db: DrizzleD1Database;
 }
 
-async function injectDB(reuqest: Request, env: Env) {
+async function injectDB(request: Request, env: Env) {
 	const db = drizzle(env.DB);
-	reuqest.db = db;
+	request.db = db;
 }
 
 interface Methods {
@@ -24,7 +24,7 @@ const router = Router<Request, Methods>({ base: '/' });
 
 // GET collection index
 router.get('/api/todos', injectDB, async (request: Request, env: Env) => {
-	const results = await request.db.select().from(records).where(gte(records.created, "2023-09-01 00:00:00"));
+	const results = await request.db.select().from(records).where(gte(records.created, '2023-09-01 00:00:00'));
 	return Response.json(results);
 });
 
@@ -38,24 +38,29 @@ router.get('/api/todos/:date', injectDB, async (request: Request, env: Env) => {
 router.post('/api/todos/:date', injectDB, async (request: Request, env: Env) => {
 	const content = await request.json();
 	let item = [];
-	console.log(content);
-	// for (const key of Object.keys(content.record)) {
-	// 	item.push(`'$.${key}'`);
-	// 	item.push(content.record[key]);
-	// }
-	// console.log("item => ",item)
-	console.log(request.params.date)
+	console.log(request.params.date);
 	const results = await request.db.select().from(records).where(eq(records.created, request.params.date));
-	console.log("results => ", results);
-	if (results.length > 0) {
-		// update
+	console.log('results => ', results);
+	console.log(request.params.date, 'shit...');
+	let result = null;
+	if (results.length == 1) {
+		const record = results[0];
+		Object.keys(content).forEach((key: string) => {
+			if (Object.keys(record.record).includes(key)) {
+				content[key] += record.record[key];
+			}
+		});
+		result = await request.db
+			.update(records)
+			.set({
+				record: content,
+			})
+			.where(eq(records.created, request.params.date))
+			.returning();
+	} else {
+		result = await request.db.insert(records).values({ record: content, created: request.params.date, user_id: 1 }).returning();
 	}
-	// const json_value = item.join(",");
-	// const info = await request.db.prepare(
-	// 	`insert into sports (record, created) values (JSON_INSERT('{}', ${json_value}), '${content.date}')`
-	// ).run();
-
-	return new Response('Creating Todo: ' + JSON.stringify(content));
+	return new Response(JSON.stringify(result));
 });
 
 // 404 for everything else
